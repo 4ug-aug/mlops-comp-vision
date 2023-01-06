23f319d601b26e800423d8f726fa79b4a3d4ed5d
4ug-aug <augusttollerup@hotmail.com>
2023-01-06 10:29:45 +0100

    Refact: Fixed wrong model definition

--- src/models/model.py
+++ src/models/model.py
@@ -5,11 +5,15 @@ import torch.nn.functional as F
 import timm
 
 
-class MyAwesomeModel(nn.Module):
-    def __init__(self):
+class MyAwesomeModel():
+    def __init__(self, classes=10):
+        self.classes = classes
+        pass
+
+    def model(self):
         model_name = "xception"
 
-        timm_model = timm.create_model(model_name, pretrained=True)
+        timm_model = timm.create_model(model_name, pretrained=False, num_classes=self.classes)
 
         return timm_model
         
