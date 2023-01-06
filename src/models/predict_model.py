f818b4e4e88cbf721413ef7d15051c562d74da24
4ug-aug <augusttollerup@hotmail.com>
2023-01-06 11:06:17 +0100

    Feat: Evaluate model functionality

--- src/models/predict_model.py
+++ src/models/predict_model.py
@@ -21,35 +21,44 @@ latest_model = get_latest_model()
 @click.command()
 @click.option("--model_path", default=latest_model, help='path to model', prompt=True)
 @click.option("--data_path", default="data/processed/test.pt", help='path to data', prompt=True)
+@click.option("--dev", default=True, help='use dev set for testing')
 
-def predict(model_path, data_path):
+def predict(model_path, data_path, dev):
     logger = logging.getLogger(__name__)
 
     logger.info('Testing model')
     logger.info(f"Model path: {model_path}")
     logger.info(f"Data path: {data_path}")
 
-    # load data
-    test_set = torch.load(data_path)
-    testloader = torch.utils.data.DataLoader(test_set, batch_size=64, shuffle=True)
+    # Load mnist/data/processed/trainset.pt
+    test_dataset = torch.load("data/processed/test_dev.pt")
 
-    # load model
-    model = torch.load(model_path)
+    print("Testing dataset loaded")
+    # print train data size
+    print("Testing dataset size: {}".format(len(test_dataset)))
 
-    # predict
-    model.eval()
+    if dev:
+        print("Using dev set for testing, taking first 100 samples")
+        test_dataset = torch.load("data/processed/test_dev.pt")
+        print("Dev dataset loaded")
+        print("Dev dataset size: {}".format(len(test_dataset)))
+
+    test_loader  = create_loader(test_dataset, input_size=(3, 32, 32), batch_size=64, use_prefetcher=False, 
+                              is_training=False, no_aug=True, transform=transform)
+
+    model = MyAwesomeModel(classes=10).model()
 
     criterion = nn.NLLLoss()
 
     with torch.no_grad():
         accuracy = 0
-        for images, labels in testloader:
+        for i, (inputs, targets) in tk0:
     
-            log_ps = model(images)
+            log_ps = model(inputs)
 
             ps = torch.exp(log_ps)
             top_p, top_class = ps.topk(1, dim=1)
-            equals = top_class == labels.view(*top_class.shape)
+            equals = top_class == targets.view(*top_class.shape)
             accuracy += torch.mean(equals.type(torch.FloatTensor))
         
     print("Accuracy: ", accuracy.item()/len(testloader)*100)
