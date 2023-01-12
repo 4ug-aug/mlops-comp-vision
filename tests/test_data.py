26886004af189972d9ec44bdb866be4476c4003a
Mads-Ringsted <mads@ringsted.com>
2023-01-12 16:08:19 +0100

    Changes to test

--- tests/test_data.py
+++ tests/test_data.py
@@ -6,43 +6,26 @@ import os
 
 def test_data_shape():
 
-    trainset, testset = cifar()
+    trainset, testset = 
     assert trainset.data.shape == (len(trainset), 32, 32, 3)
     assert testset.data.shape == (len(testset), 32, 32, 3)
-    
-    train_dev, test_dev = cifar(dev=True)
-    train_dev, test_dev = trainset.data[train_dev.indices], testset.data[test_dev.indices]
-    assert train_dev.data.shape == (len(train_dev), 32, 32, 3)
-    assert test_dev.data.shape == (len(test_dev), 32, 32, 3)
 
 def test_data_size():
-
-    trainset, testset = cifar()
-    assert len(trainset) == 50000
-    assert len(testset) == 10000
-
-    train_dev, test_dev = cifar(dev=True)
-    assert len(train_dev) == 5000
-    assert len(test_dev) == 1000
+    
+    assert len(trainset) == 12639
+    assert len(testset) == 500
+    assert len(valset) == 500
 
 def test_classes_represented():
     
-    trainset, testset = cifar()
-    train_dev, test_dev = cifar(dev=True)
-    
-    train_dev = torch.utils.data.DataLoader(train_dev)
-    test_dev = torch.utils.data.DataLoader(test_dev)
-    train_dev_targets = torch.tensor([])
-    test_dev_targets = torch.tensor([])
-    for _, labels in train_dev:
-        train_dev_targets = torch.cat([train_dev_targets, labels], dim=0)
-    for _ , labels in test_dev:
-        test_dev_targets = torch.cat([test_dev_targets, labels], dim=0)
-    
 
-    target_list = [torch.tensor(trainset.targets), torch.tensor(testset.targets), train_dev_targets, test_dev_targets]
-    for targets in target_list:
-        assert torch.eq(torch.unique(targets), torch.tensor(range(10))).all(), f'One of the datasets do not contain all labels'
+    train_labels =
+    test_labels =
+    val_labels =
+
+    assert torch.eq(torch.unique(train_labels), torch.tensor(range(10))).all(), f'One of the datasets do not contain all labels'
+    assert torch.eq(torch.unique(test_labels), torch.tensor(range(10))).all(), f'One of the datasets do not contain all labels'
+    assert torch.eq(torch.unique(val_labels), torch.tensor(range(10))).all(), f'One of the datasets do not contain all labels'
 
 
 
