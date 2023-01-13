77bef947ab35015c829055aa5764770bef58e9c2
Mads-Ringsted <mads@ringsted.com>
2023-01-13 10:59:59 +0100

    small changes

--- src/data/load_data.py
+++ src/data/load_data.py
@@ -1,8 +1,8 @@
 import torch
 
 def load_data():
-    trainset = torch.load("/data/processed/train.pt")
-    testset = torch.load("/data/processed/test.pt")
-    valset = torch.load("/data/processed/val.pt")
+    trainset = torch.load("data/processed/train.pt")
+    testset = torch.load("data/processed/test.pt")
+    valset = torch.load("data/processed/val.pt")
 
     return trainset, testset, valset
