6371c470fa56fb825aa6fad9f60ebdaaf17987aa
marcusroberto02 <marcusrobertonielsen02@gmail.com>
2023-01-17 16:44:54 +0100

    removed boilerplate through pytorch lightning

---
+++ src/models-lightning/model.py
@@ -0,0 +1,71 @@
+import torch
+from torch.utils.data import DataLoader
+from torch import nn, optim
+import timm
+from pytorch_lightning import LightningModule
+from torchmetrics.functional import accuracy
+
+
+class MyAwesomeModel(LightningModule):
+    def __init__(self, model_name="resnet18",classes=10, num_workers=1, pretrained=True):
+        super().__init__()
+
+        self.model_name = model_name
+        self.classes = classes
+        self.num_workers = num_workers
+        self.pretrained = pretrained
+
+        self.cnn = timm.create_model(self.model_name,pretrained=self.pretrained,num_classes = self.classes)
+
+        self.criterion = nn.CrossEntropyLoss()
+
+    def forward(self,x):
+        return self.cnn(x)
+        
+    def training_step(self, batch, batch_idx):
+        data, target = batch
+        preds = self(data)
+        loss = self.criterion(preds,target)
+        acc = (target == preds.argmax(dim=-1)).float().mean()
+        self.log('train_loss', loss)
+        self.log('train_acc', acc)
+        return loss
+    
+    def test_step(self, batch, batch_idx):
+        loss, acc = self._shared_eval_step(batch, batch_idx)
+        metrics = {"test_acc": acc, "test_loss": loss}
+        self.log_dict(metrics)
+        return metrics
+
+    def _shared_eval_step(self, batch, batch_idx):
+        x, y = batch
+        y_hat = self(x)
+        loss = self.criterion(y_hat, y)
+        acc = accuracy(y_hat, y, task="multiclass",num_classes=self.classes)
+        return loss, acc
+    
+    def validation_step(self, batch, batch_idx):
+        x, y = batch
+        y_hat = self(x)
+        loss = self.criterion(y_hat, y)
+        self.log("val_loss", loss)
+    
+    def configure_optimizers(self):
+        return optim.Adam(self.parameters(),lr=1e-2)
+
+    def train_dataloader(self):
+        trainset = torch.load("data/processed/train.pt")
+        return DataLoader(trainset,batch_size=64,shuffle=True,num_workers=self.num_workers)
+
+    def test_dataloader(self):
+        testset = torch.load("data/processed/test.pt")
+        return DataLoader(testset,batch_size=64,shuffle=True,num_workers=self.num_workers)
+
+    def val_dataloader(self):
+        valset = torch.load("data/processed/val.pt")
+        return DataLoader(valset,batch_size=64,num_workers=self.num_workers)
+
+
+
+        
+
